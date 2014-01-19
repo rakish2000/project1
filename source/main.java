950ee0b2f2e81b8d216442229b8e1dc88dc3749d
Test20142014 <mymail666000@gmail.com>
2014-01-19 15:15:48 -0500

    Update main.java

--- source/main.java
+++ source/main.java
@@ -1,9 +1,14 @@
 
-
 public class hello
 {
   public static void main(String [] args)
   {
     System.out.println("Hello") ;
+  }
+  
+  
+  public static boolean cal()
+  {
+    return true ;
   }
 }
